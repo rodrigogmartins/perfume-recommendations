3daf34f99616716b0032c5cdf6bc2a8b36a4e6e0
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2025-05-09 01:06:21 -0300

    refact: saving rating and pretty name

--- src/data/fill_perfume_dataset.py
+++ src/data/fill_perfume_dataset.py
@@ -1,11 +1,10 @@
 import pandas as pd
-import uuid
-from pymongo import MongoClient, ASCENDING
+from pymongo import MongoClient, ASCENDING, DESCENDING
 
 from src.data.inferences.perfume_climates_usage_recommendations_inference import get_climates_recommendations_inference
-from src.data.inferences.perfume_seasons_usage_recommendations_inference import get_seasons_recommendations_inference
 from src.data.inferences.perfume_day_shifts_usage_recommendations_inference import \
     get_day_shifts_recommendations_inference
+from src.data.inferences.perfume_seasons_usage_recommendations_inference import get_seasons_recommendations_inference
 
 client = MongoClient("mongodb://root:example@localhost:27017/")
 db = client["perfume_db"]
@@ -18,21 +17,33 @@ collection.create_index([("accords", ASCENDING)])
 collection.create_index([("day_shifts", ASCENDING)])
 collection.create_index([("climates", ASCENDING)])
 collection.create_index([("seasons", ASCENDING)])
+collection.create_index([("rating", DESCENDING)])
 
-df = pd.read_csv("datasets/fra_cleaned.csv", sep=";", encoding="latin1")
+perfumes_data_cleaned = pd.read_csv("datasets/fra_cleaned.csv", sep=";", encoding="latin1")
+perfumes_data = pd.read_csv("datasets/fra_perfumes.csv", sep=",", encoding="latin1").to_dict(orient="records")
 
 perfumes = []
+url_index = {
+    item["url"].strip().lower(): item
+    for item in perfumes_data
+    if "url" in item and item["url"]
+}
 
 def get_accords(row_param):
     accords_list = [row_param[f"mainaccord{i}"] for i in range(1, 6) if pd.notna(row_param[f"mainaccord{i}"])]
     return [a.lower().strip() for a in accords_list]
 
-for _, row in df.iterrows():
+for index, row in perfumes_data_cleaned.iterrows():
     try:
+        item = url_index.get(row["url"].strip().lower())
+
+        if (item is None) or (item == ""):
+            continue
+
         accords = get_accords(row)
         perfume = {
-            "_id": str(uuid.uuid4()),
-            "name": row["Perfume"].strip(),
+            "_id": str(index),
+            "name": item["Name"].replace(item["Gender"], ""),
             "brand": row["Brand"].strip(),
             "country": row["Country"].strip(),
             "gender": row["Gender"].strip(),
@@ -45,7 +56,8 @@ for _, row in df.iterrows():
             "day_shifts": get_day_shifts_recommendations_inference(accords),
             "climates": get_climates_recommendations_inference(accords),
             "seasons": get_seasons_recommendations_inference(accords),
-            "url": row["url"].strip()
+            "url": row["url"].strip(),
+            "rating": item["Rating Value"]
         }
         perfumes.append(perfume)
     except Exception as e:
