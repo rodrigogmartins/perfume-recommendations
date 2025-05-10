501ce331000eef7e3807d077611f25be55d6d05a
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2025-05-10 11:20:01 -0300

    feat: add endpoint to search perfumes using full-text search

--- src/controller/perfumes_controller.py
+++ src/controller/perfumes_controller.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 
 from src.perfumes.dto.user_input_query import UserInputQuery
-from src.perfumes.get_perfumes import get_sorted_perfumes
+from src.perfumes.get_perfumes_recommendations import get_sorted_perfumes
+from src.perfumes.search_perfumes_by_text import search_perfumes_by_text
 from src.perfumes.utils.user_query_input_builder import user_input_query_builder
 
 app = FastAPI()
+
+
+@app.get("/api/perfumes/search")
+def search_perfumes(query: str = Query(..., min_length=2), limit: int = 10):
+    results = search_perfumes_by_text(query, limit)
+    return results
 
 @app.post("/api/perfumes/recommendations")
 async def perfumes_recommendations(user_input_query: UserInputQuery):
---
+++ src/perfumes/search_perfumes_by_text.py
@@ -0,0 +1,19 @@
+from src.cross.mongo_client import get_mongo_collection
+
+def search_perfumes_by_text(query, limit = 10):
+    collection = get_mongo_collection("perfumes")
+    return list(
+        collection.find(
+            { "$text": { "$search": query } },
+            {
+                "score": { "$meta": "textScore" },
+                "name": 1,
+                "brand": 1,
+                "url": 1,
+                "image_url": 1,
+                "rating": 1,
+            }
+        )
+        .sort([("score", {"$meta": "textScore"})])
+        .limit(limit)
+    )
