5b8a46d34e9cc1336f7b3fb6cfdd77929ae7b0c6
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2025-05-09 22:38:46 -0300

    refact: updating affinity score calculation

--- src/perfumes/calculate_affinity_score.py
+++ src/perfumes/calculate_affinity_score.py
@@ -7,14 +7,14 @@ def calculate_affinity_score(perfume, user_preferences):
     perfume_notes = set(perfume.get("all_notes", []))
     perfume_accords = set(perfume.get("accords", []))
 
-    score += len(liked_notes & perfume_notes) * 3
-    score += len(liked_accords & perfume_accords) * 2
+    score += len(liked_notes & perfume_notes) * 6
+    score += len(liked_accords & perfume_accords) * 4
     max_score += max(len(liked_notes), 1) * 3 + max(len(liked_accords), 1) * 2
 
     not_liked_notes = set(user_preferences["notLikedNotes"])
     not_liked_accords = set(user_preferences["notLikedAccords"])
-    score -= len(not_liked_notes & perfume_notes) * 5
-    score -= len(not_liked_accords & perfume_accords) * 4
+    score -= len(not_liked_notes & perfume_notes) * 3
+    score -= len(not_liked_accords & perfume_accords) * 2
 
     prob = max(min(score / max(max_score, 1), 1), 0)
 
