a6b350149ccb932a5d7391c8ebd998d59ff5c989
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2025-05-16 15:51:32 -0300

    infra: terraform create mongodb

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+  name       = "docdb-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "DocDB Subnet Group"
+  }
+}
+
 data "aws_secretsmanager_secret_version" "db_credentials" {
   secret_id = var.secret_arn
 }
@@ -10,21 +19,30 @@ locals {
   secret_data = jsondecode(data.aws_secretsmanager_secret_version.db_credentials.secret_string)
 }
 
-resource "aws_docdb_cluster" "default" {
-  cluster_identifier = var.cluster_id
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier = var.db_cluster_identifier
   engine             = "docdb"
   master_username    = local.secret_data[var.secret_username_key]
   master_password    = local.secret_data[var.secret_password_key]
+  db_subnet_group_name         = aws_docdb_subnet_group.docdb_subnet_group.name
+  vpc_security_group_ids       = var.vpc_security_group_ids
+  skip_final_snapshot          = true
+  backup_retention_period      = 0
+  deletion_protection          = false
+  apply_immediately            = true
 
-  backup_retention_period = 0
-  skip_final_snapshot     = true
+  tags = {
+    Name = "MainDocDBCluster"
+  }
 }
 
-resource "aws_docdb_cluster_instance" "default" {
-  identifier          = "${var.cluster_id}-instance-1"
-  cluster_identifier  = aws_docdb_cluster.default.id
-  instance_class      = var.instance_class
-  engine              = "docdb"
-  engine_version      = "4.0.0"
-  apply_immediately   = true
+resource "aws_docdb_cluster_instance" "main_instance" {
+  identifier         = "${var.db_cluster_identifier}-instance"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.db_instance_class
+  apply_immediately  = true
+
+  tags = {
+    Name = "MainDocDBInstance"
+  }
 }
